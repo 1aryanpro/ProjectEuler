bbc3784872b7eb3c501c0afedb7cf110d2b39486
smashMineGame <aryanp@khanlabschool.org>
2021-06-06 23:12:23 -0700

    Started First Exercise

---
+++ GenerateLanguages.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import random
+
+knowLangs = ["JS", "Python", "C++", "Java"]
+newLangs = ["Go", "Rust", "Objective C", "C#", "Swift"]
+
+print("Know Lang: " + random.choice(knowLangs))
+print("New Langs: " + random.choice(newLangs))
